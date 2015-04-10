d632f2531a7ef26bacf997f351c513adeab9a067
Zachary Graber <zackee12@gmail.com>
2015-04-10 00:49:36 -0700

    refactor print score for readability and update comments

--- util/printer.py
+++ util/printer.py
@@ -2,13 +2,29 @@ from util.enums import Player
 
 
 def print_moves(board):
+    """ Print all valid moves in long algebraic form
+
+    :param board: Board class
+    :return: None
+    """
     for move in board.valid_moves():
         print(str(move))
 
 
 def print_score(board):
-    print('White: {}\nBlack: {}'.format(board.score(Player.WHITE), board.score(Player.BLACK)))
+    """ Print score for both players
+
+    :param board: board class
+    :return: None
+    """
+    print('White: {}'.format(board.score(Player.WHITE)))
+    print('Black: {}'.format(board.score(Player.BLACK)))
 
 
 def print_board(board):
+    """ Print chessboard
+
+    :param board: board class
+    :return: None
+    """
     print(board)
