5c7e4fce2238d77f9dc4cf3281c49b0c61327b96
Zachary Graber <zackee12@gmail.com>
2015-04-10 00:17:23 -0700

    update comments

--- board/move.py
+++ board/move.py
@@ -2,7 +2,18 @@ from util.enums import Side
 
 
 class Move:
+    """
+    Data structure to hold needed information relating to a chess move
+    """
     def __init__(self, piece, location, captured_piece=None):
+        """ initializer
+
+        :param piece: Piece class
+        :param location: Location class
+        :param captured_piece: Piece class
+        :return:
+        """
+        # standard moves
         self.piece = piece
         self.old_location = piece.location
         self.new_location = location
@@ -23,6 +34,7 @@ class Move:
         self.promotion_piece_class = None
 
     def __str__(self):
+        """ Long algebraic notation"""
         if self.castle:
             if self.castle_side == Side.KING:
                 return 'O-O'
@@ -41,6 +53,7 @@ class Move:
 
     def __eq__(self, other):
         if self.__class__ is other.__class__:
+            # compare all dict keys
             o = vars(other)
             for k, v in vars(self).items():
                 if k not in o or o[k] != v:
@@ -50,11 +63,20 @@ class Move:
 
     @classmethod
     def create_castle(cls, king, king_location, rook, rook_location):
+        """ Creates a castle move
+        :param king: King class
+        :param king_location: Location class for new location of king
+        :param rook: Rook class
+        :param rook_location: Location class for new location of rook
+        :return: Move
+        """
         move = cls(king, king_location)
         move.castle = True
         move.rook_piece = rook
         move.rook_old_location = rook.location
         move.rook_new_location = rook_location
+
+        # determine if king side or queen side castle
         if king_location.col > king.location.col:
             move.castle_side = Side.KING
         else:
@@ -63,12 +85,27 @@ class Move:
 
     @classmethod
     def create_en_passant(cls, pawn_piece, pawn_new_location, captured_pawn_piece):
+        """ Create an en passant pawn move
+
+        :param pawn_piece: Pawn
+        :param pawn_new_location: Location class for new location of pawn
+        :param captured_pawn_piece: Pawn
+        :return: Move
+        """
         move = cls(pawn_piece, pawn_new_location, captured_pawn_piece)
         move.en_passant = True
         return move
 
     @classmethod
     def create_promotion(cls, pawn_piece, pawn_new_location, promoted_piece_class, captured_piece=None):
+        """ Create a pawn promotion move
+
+        :param pawn_piece: Pawn
+        :param pawn_new_location: Location class for new location of pawn
+        :param promoted_piece_class: class of the desired piece to promote pawn to
+        :param captured_piece: Piece class for any piece that is captured during move
+        :return: Move
+        """
         move = cls(pawn_piece, pawn_new_location, captured_piece)
         move.promotion = True
         move.promotion_piece_class = promoted_piece_class
