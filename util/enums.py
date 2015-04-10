c6614c7591104f3ccd7abc89de8d70ed62bbd04a
Zachary Graber <zackee12@gmail.com>
2015-04-10 00:38:44 -0700

    refactor player enum

--- util/enums.py
+++ util/enums.py
@@ -5,15 +5,8 @@ class Player(enum.Enum):
     WHITE = 0
     BLACK = 1
 
-    def __init__(self, val):
-        self.val = val # boolean
-
-    #@classmethod
-    #def opponent(cls, player):
-    #    return Player.WHITE if player == Player.BLACK else Player.BLACK
-
     def opponent(self):
-        return Player.WHITE if self.val == Player.BLACK.value else Player.BLACK
+        return Player.WHITE if self.value == Player.BLACK.value else Player.BLACK
 
 
 class Color(enum.Enum):
