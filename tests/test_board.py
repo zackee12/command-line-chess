cfe2a9e5420b4271d6f02cfeae228c38a8677afc
Zachary Graber <zackee12@gmail.com>
2015-04-11 00:37:21 -0700

    add tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -2,17 +2,22 @@ from unittest import TestCase
 
 from board import Board
 from util.input_parser import parse
-from util.enums import Player
-from piece import Pawn, Queen, Rook, Bishop, Knight
+from util.enums import Player, Side
+from piece import Pawn, Queen, Rook, Bishop, Knight, King
+from board.location import Location
+from util.printer import print_moves
 
 
 class TestBoard(TestCase):
     def setUp(self):
         self.board = Board()
+        self.board2 = Board(False)
+        King(Location(8, 'e'), Player.BLACK, self.board2)
+        King(Location(1, 'e'), Player.WHITE, self.board2)
 
-    def batch_move(self, moves):
+    def batch_move(self, board, moves):
         for move in moves:
-            move = parse(self.board, move)
+            move = parse(board, move)
             self.board.move(move)
 
     def test_two_move_checkmate(self):
@@ -22,9 +27,36 @@ class TestBoard(TestCase):
             'g2 g4',
             'd8 h4',
         ]
-        self.batch_move(moves)
+        self.batch_move(self.board, moves)
 
         self.assertTrue(self.board.checkmate())
+
+    def test_draw(self):
+        moves = [
+            'c2 c4',
+            'h7 h5',
+            'h2 h4',
+            'a7 a5',
+            'd1 a4',
+            'a8 a6',
+            'a4 a5',
+            'a6 h6',
+            'a5 c7',
+            'f7 f6',
+            'c7 d7',
+            'e8 f7',
+            'd7 b7',
+            'd8 d3',
+            'b7 b8',
+            'd3 h7',
+            'b8 c8',
+            'f7 g6',
+            'c8 e6',
+        ]
+        self.assertFalse(self.board.draw())
+        self.batch_move(self.board, moves)
+        self.assertTrue(self.board.draw())
+        self.assertFalse(self.board.checkmate())
 
     def test_en_passant(self):
         moves = [
@@ -33,7 +65,7 @@ class TestBoard(TestCase):
             'a4 a5',
             'b7 b5',
         ]
-        self.batch_move(moves)
+        self.batch_move(self.board, moves)
 
         move = parse(self.board, 'a5 b6')  # white en passant attack
         self.assertTrue(move.en_passant)
@@ -51,12 +83,48 @@ class TestBoard(TestCase):
             'f1 g2',
             'c7 c6',
         ]
-        self.batch_move(moves)
+        self.batch_move(self.board, moves)
 
         move = parse(self.board, 'e1 g1')  # white
         self.assertTrue(move.castle)
         self.assertTrue(move in self.board.valid_moves())
+        self.assertEquals(move.castle_side, Side.KING)
+        self.assertEquals(str(move), 'O-O')
         self.board.move(move)
+
+    def test_queen_side_castle(self):
+        moves = [
+            'b1 a3',
+            'a7 a6',
+            'b2 b4',
+            'b7 b6',
+            'c1 b2',
+            'c7 c6',
+            'c2 c4',
+            'd7 d6',
+            'd1 c2',
+            'e7 e6',
+        ]
+        self.batch_move(self.board, moves)
+
+        move = parse(self.board, 'e1 c1')  # white
+        self.assertTrue(move.castle)
+        self.assertTrue(move in self.board.valid_moves())
+        self.assertEquals(move.castle_side, Side.QUEEN)
+        self.assertEquals(str(move), 'O-O-O')
+        self.board.move(move)
+
+    def test_king_side_castle_check_in_middle(self):
+        Rook(Location(1, 'h'), Player.WHITE, self.board2)
+        Queen(Location(2, 'f'), Player.BLACK, self.board2)
+        with self.assertRaises(IOError):
+            parse(self.board2, 'e1 g1')
+
+    def test_queen_side_castle_check_in_middle(self):
+        Rook(Location(1, 'a'), Player.WHITE, self.board2)
+        Queen(Location(2, 'd'), Player.BLACK, self.board2)
+        with self.assertRaises(IOError):
+            parse(self.board2, 'e1 c1')
 
     def test_promotion(self):
         moves = [
@@ -69,7 +137,7 @@ class TestBoard(TestCase):
             'b6 a7',
             'f7 f6',
         ]
-        self.batch_move(moves)
+        self.batch_move(self.board, moves)
 
         with self.assertRaises(IOError):
             parse(self.board, 'a7 b8')
@@ -92,3 +160,57 @@ class TestBoard(TestCase):
 
         self.board.move(move)
         self.assertEquals(self.board.piece(move.new_location).__class__, Knight)
+
+    def test_random_game(self):
+        for i in range(5):
+            if self.board.checkmate():
+                break
+            if self.board.draw():
+                break
+
+            self.assertEquals(len(self.board._moves), i)
+            self.board.random_move()
+
+    def test_score(self):
+        self.assertEquals(self.board2.score(Player.WHITE), 0)
+        p = Pawn(Location(2, 'a'), Player.WHITE, self.board2)
+        self.board2.remove(p)
+        self.assertEquals(self.board2.score(Player.WHITE), Pawn.VALUE)
+        k = Knight(Location(3, 'a'), Player.WHITE, self.board2)
+        self.board2.remove(k)
+        self.assertEquals(self.board2.score(Player.WHITE), Pawn.VALUE + Knight.VALUE)
+
+    def test_last_move(self):
+        self.assertEquals(self.board2.last_move(), None)
+
+    def test_pieces(self):
+        all_pieces = list(self.board.pieces())
+        self.assertEquals(all_pieces, self.board._pieces)
+        kings = list(self.board.pieces(piece_class=King))
+        self.assertTrue(all([king.__class__ is King for king in kings]))
+        white = list(self.board.pieces(player=Player.WHITE))
+        self.assertTrue(all([p.player is Player.WHITE for p in white]))
+        white_knights = list(self.board.pieces(piece_class=Knight, player=Player.WHITE))
+        self.assertTrue(all([p.player is Player.WHITE and p.__class__ is Knight for p in white_knights]))
+
+    def test_str(self):
+        s = "-----------------------------------\n" \
+            "8 | R | N | B | Q | K | B | N | R |\n" \
+            "-----------------------------------\n" \
+            "7 | P | P | P | P | P | P | P | P |\n" \
+            "-----------------------------------\n" \
+            "6 |   |   |   |   |   |   |   |   |\n" \
+            "-----------------------------------\n" \
+            "5 |   |   |   |   |   |   |   |   |\n" \
+            "-----------------------------------\n" \
+            "4 |   |   |   |   |   |   |   |   |\n" \
+            "-----------------------------------\n" \
+            "3 |   |   |   |   |   |   |   |   |\n" \
+            "-----------------------------------\n" \
+            "2 | p | p | p | p | p | p | p | p |\n" \
+            "-----------------------------------\n" \
+            "1 | r | n | b | q | k | b | n | r |\n" \
+            "-----------------------------------\n" \
+            "  | a | b | c | d | e | f | g | h |\n"
+        self.board.character_map.unicode = False
+        self.assertEquals(str(self.board), s)
--- tests/test_location.py
+++ tests/test_location.py
@@ -9,6 +9,9 @@ class TestLocation(TestCase):
         with self.assertRaises(ValueError):
             l = Location(50, 'a')
 
+        with self.assertRaises(ValueError):
+            l = Location(1, 'z')
+
     def test_color(self):
         l1, l2, l3, l4 = Location(1, 'a'), Location(5, 'c'), Location(5, 'f'), Location(8, 'a')
         self.assertEquals(l1.color, Color.BLACK)
@@ -28,6 +31,24 @@ class TestLocation(TestCase):
         l = Location.from_string('a1')
         self.assertEquals(l.row, 1)
         self.assertEquals(l.col, 'a')
+
+    def test_from_string_error(self):
+        with self.assertRaises(ValueError):
+            Location.from_string('aa')
+
+        with self.assertRaises(ValueError):
+            Location.from_string('11')
+
+        with self.assertRaises(ValueError):
+            Location.from_string('111111a')
+
+    def test_get(self):
+        l = Location(1, 'a')
+        self.assertEquals(l[0], 1)
+        self.assertEquals(l[1], 'a')
+
+        with self.assertRaises(IndexError):
+            l[2]
 
     def test_from_between_rows(self):
         comp = [Location(2, 'a'), Location(3, 'a'), Location(4, 'a')]
