d3c2023bda2ee85f310fb39a6fa9c7861230a4d7
Zachary Graber <zackee12@gmail.com>
2015-04-26 01:22:32 -0700

    add comments

--- ai/minimax.py
+++ ai/minimax.py
@@ -30,12 +30,25 @@ def minimax(game, player, depth, max_depth):
 
 
 def alphabeta(game, player, depth, max_depth, alpha, beta):
+    """ Alpha beta minimax algorithm
+
+    :param game: game object with game_over, heuristic_value, and valid_moves methods
+    :param player: maximizing player
+    :param depth: current depth in tree
+    :param max_depth: maximum depth in tree to search
+    :param alpha: minimum score maximizing player will get
+    :param beta: maximum score minimizing player will get
+    :return:  score, list of moves
+    """
+    # base case - game over or depth exceeded
     if game.game_over() or depth > max_depth:
         return game.heuristic_value(player, depth), []
 
+    # init best score to worst possible
     best_score = -float('inf') if game.current_player == player else float('inf')
     best_moves = []
 
+    # loop all moves
     for move in list(game.valid_moves()):
         # make move
         game.move(move)
@@ -61,7 +74,7 @@ def alphabeta(game, player, depth, max_depth, alpha, beta):
             elif score == best_score:
                 best_moves.append(move)
 
-        # prune branch
+        # prune branch if the minimum score the max player gets exceeds the max score the min player gets
         if alpha >= beta:
             break
 
